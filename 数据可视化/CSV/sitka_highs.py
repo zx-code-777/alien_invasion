bb941f9f0d13afe756d424c6b1f2ac73696647be
zx_alien_invasion <1425690182@qq.com>
2021-12-07 21:19:41 +0800

    Changes

--- 数据可视化/CSV/sitka_highs.py
+++ 数据可视化/CSV/sitka_highs.py
@@ -1,1 +1,20 @@
 import csv
+
+filename = 'sitka_weather_07-2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+# 对例表调用enumerate()来获取每个元素的索引及其值
+# for index,column_header in enumerate(header_row):
+#     print(index,column_header)
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+print(highs)
+
+
+
+
+
