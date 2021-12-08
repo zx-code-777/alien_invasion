dd372d3610c3769fb06d0b5c3556a7f676b64bda
zx_alien_invasion <1425690182@qq.com>
2021-12-08 21:48:22 +0800

    Changes

--- 数据可视化/JSON格式/eq_explort_data.py
+++ 数据可视化/JSON格式/eq_explort_data.py
@@ -12,8 +12,14 @@ with open(filename) as f:
 #     json.dump(all_eq_data, f, indent=4)
 
 all_eq_dicts = all_eq_data['features']
-mags, title, lons, lats = [], [], [], []
+mags, titles, lons, lats = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
+    title = eq_dict['properties']['title']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
     mags.append(mag)
-print(mags[:10])
+    titles.append(title)
+    lons.append(lon)
+    lats.append(lat)
+
